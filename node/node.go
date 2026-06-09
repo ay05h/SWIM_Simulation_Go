82294802f533550e9922d2fcf2ecf22fecc86fbd
Ayush Kumar Rai <ayushrai4629@gmail.com>
2026-06-09 20:23:04 +0530

    Add node ping/ack event loop

--- node/node.go
+++ node/node.go
@@ -1,11 +1,15 @@
 package node
 
 import (
-	"clusterpulse/membership"
-	"clusterpulse/protocol"
+	"context"
+	"fmt"
+	"log"
 	"math/rand"
 	"sort"
 	"time"
+
+	"clusterpulse/membership"
+	"clusterpulse/protocol"
 )
 
 type Config struct {
@@ -21,8 +25,9 @@ type Node struct {
 	probeInterval time.Duration
 	ackTimeout    time.Duration
 	rng           *rand.Rand
-	table  *membership.Table
+	table         *membership.Table
 	inbox         chan protocol.Message
+	outbox        chan protocol.Message
 }
 
 func New(cfg Config) *Node {
@@ -67,8 +72,9 @@ func New(cfg Config) *Node {
 				time.Now().UnixNano() + int64(len(cfg.ID))*7919,
 			),
 		),
-		inbox: make(chan protocol.Message, bufferSize),
-		table: membership.NewTable(cfg.ID, cfg.KnownNodes),
+		inbox:  make(chan protocol.Message, bufferSize),
+		outbox: make(chan protocol.Message, bufferSize),
+		table:  membership.NewTable(cfg.ID, cfg.KnownNodes),
 	}
 }
 
@@ -80,3 +86,93 @@ func (n *Node) Inbox() chan protocol.Message {
 	return n.inbox
 }
 
+func (n *Node) Outbox() <-chan protocol.Message {
+	return n.outbox
+}
+
+func (n *Node) Run(ctx context.Context, logger *log.Logger) {
+	ticker := time.NewTicker(n.probeInterval)
+	defer ticker.Stop()
+
+	logf(logger, "[%s] started", n.id)
+
+	for {
+		select {
+		case <-ctx.Done():
+			logf(logger, "[%s] stopped", n.id)
+			return
+		case <-ticker.C:
+			n.probeRandomPeer(ctx, logger)
+		case msg, ok := <-n.inbox:
+			if !ok {
+				logf(logger, "[%s] inbox closed", n.id)
+				return
+			}
+			n.handleMessage(ctx, msg, logger)
+		}
+	}
+}
+
+func (n *Node) probeRandomPeer(ctx context.Context, logger *log.Logger) {
+	if len(n.peers) == 0 {
+		return
+	}
+
+	peer := n.peers[n.rng.Intn(len(n.peers))]
+	msg := protocol.Message{
+		Type:          protocol.MessagePing,
+		From:          n.id,
+		To:            peer,
+		CorrelationID: n.newCorrelationID(),
+		SentAt:        time.Now(),
+	}
+
+	logf(logger, "[%s] probing %s (%s)", n.id, peer, msg.CorrelationID)
+	n.send(ctx, msg)
+}
+
+func (n *Node) handleMessage(ctx context.Context, msg protocol.Message, logger *log.Logger) {
+	switch msg.Type {
+	case protocol.MessagePing:
+		logf(logger, "[%s] received PING from %s (%s)", n.id, msg.From, msg.CorrelationID)
+		n.sendAck(ctx, msg, logger)
+	case protocol.MessageAck:
+		logf(logger, "[%s] received ACK from %s (%s)", n.id, msg.From, msg.CorrelationID)
+	default:
+		logf(logger, "[%s] ignored %s from %s (%s)", n.id, msg.Type, msg.From, msg.CorrelationID)
+	}
+}
+
+func (n *Node) sendAck(ctx context.Context, ping protocol.Message, logger *log.Logger) {
+	ack := protocol.Message{
+		Type:          protocol.MessageAck,
+		From:          n.id,
+		To:            ping.From,
+		CorrelationID: ping.CorrelationID,
+		SentAt:        time.Now(),
+	}
+
+	logf(logger, "[%s] acknowledging %s (%s)", n.id, ping.From, ping.CorrelationID)
+	n.send(ctx, ack)
+}
+
+func (n *Node) send(ctx context.Context, msg protocol.Message) bool {
+	select {
+	case <-ctx.Done():
+		return false
+	case n.outbox <- msg:
+		return true
+	}
+}
+
+func (n *Node) newCorrelationID() string {
+	return fmt.Sprintf("%s-%d-%d", n.id, time.Now().UnixNano(), n.rng.Int63())
+}
+
+func logf(logger *log.Logger, format string, args ...any) {
+	if logger == nil {
+		return
+	}
+
+	logger.Printf(format, args...)
+}
